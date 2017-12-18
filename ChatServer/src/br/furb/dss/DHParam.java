6e351c6cbcf54a9182bdd56456976b3de7f0adf0
johnnywiller <johnnywiller10@gmail.com>
2017-12-18 21:21:01 -0200

    fff

---
+++ ChatServer/src/br/furb/dss/DHParam.java
@@ -0,0 +1,23 @@
+package br.furb.dss;
+
+public class DHParam {
+	
+	byte[] signature;
+	byte[] content;
+	public byte[] getSignature() {
+		return signature;
+	}
+	public void setSignature(byte[] signature) {
+		this.signature = signature;
+	}
+	public byte[] getContent() {
+		return content;
+	}
+	public void setContent(byte[] content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+}
